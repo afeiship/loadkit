da2fc66d06db75fd7535d55f2ff3743be12ba920
afeiship <1290657123@qq.com>
2022-11-03 22:27:25 +0800

    fix: trigger multiple times

--- packages/loadkit/src/load-node.ts
+++ packages/loadkit/src/load-node.ts
@@ -11,17 +11,28 @@ export interface LoadNodeOptions {
 export default function loadNode(inUrl: string, inProps: LoadNodeOptions): Promise<null> {
   const { id, urlKey, tagName, callback, container, ...opts } = inProps;
   const containerNode = container || document.body;
-  if (id && document.getElementById(id)) return Promise.resolve(null);
-
   const props = { [urlKey!]: inUrl, ...opts };
+  const elSelector = `${tagName}[${urlKey}="${inUrl}"]`;
+  const cleanUp = () => {
+    const els = document.querySelectorAll(elSelector) as any;
+    if (els.length > 1) {
+      // remove all but the first
+      for (let i = 1; i < els.length; i++) {
+        const el = els[i]!;
+        el.parentNode.removeChild(els[i]);
+      }
+    }
+  };
 
   let node = document.createElement(tagName!);
   if (id) node.id = id;
-  callback && (node = callback(node));
+  if (callback) node = callback(node);
+
   for (const key in props) node.setAttribute(key, props[key]);
   containerNode.appendChild(node);
+
   return new Promise((resolve, reject) => {
-    node.onload = () => resolve(null);
+    node.onload = () => (cleanUp(), resolve(null));
     node.onerror = () => reject(new Error(`Failed to load node: ${inUrl}`));
   });
 }
